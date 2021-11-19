ba9a8b8d40884ed6236a4bd675c85facd0f745c9
Art <arthurluiz36@gmail.com>
2021-11-19 18:39:54 -0200

    importando e exibindo a lista de itens atraves dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import * as C from './App.styles'
+import {useState} from 'react'
+import { item } from './types/item'
+import { ListItem } from './components/ListItem'
+
 
 const App = () =>{
+
+  const [list, setList] = useState<item[]>([
+    {id: 1, name: 'Comprar Pão', done: false},
+    {id: 2, name: 'Comprar Bolo', done: false}
+  ]);
+
   return(
     <C.Container>
         <C.Area>
           <C.Header>Lista de Tarefas</C.Header>
+
+          {/*Área para adicionar novos itens*/ }
+
+          {list.map((item,index) => (
+            <ListItem key = {index} item = {item}/>
+          ))}
+
         </C.Area>
     </C.Container>
   );
