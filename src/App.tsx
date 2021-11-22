d0a963f2995cff4fe980a8b52b79f6199f269afd
Art <arthurluiz36@gmail.com>
2021-11-22 18:05:14 -0200

    Criaçao da funcao que irá setar itens na lista

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,22 @@ const App = () =>{
     {id: 3, name: 'Comprar Salgados', done: false}
   ]);
 
+    const handleAddTask = (taskName : string) =>{
+      let newList = [...list];
+      newList.push({
+        id: list.length + 1,
+        name: taskName,
+        done: false
+      })
+      setList(newList)
+    }
+
   return(
     <C.Container>
         <C.Area>
           <C.Header>Lista de Tarefas</C.Header>
 
-          <AddArea/>
+          <AddArea onEnter = {handleAddTask}/>
 
           {list.map((item,index) => (
             <ListItem key = {index} item = {item}/>
