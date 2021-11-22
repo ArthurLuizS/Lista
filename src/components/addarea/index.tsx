d4d5eae92961eeab55801bdca8d9256d74694b34
Art <arthurluiz36@gmail.com>
2021-11-22 17:59:09 -0200

    Encaminhando a props para o app.tsx

--- src/components/addarea/index.tsx
+++ src/components/addarea/index.tsx
@@ -1,12 +1,34 @@
 import *as C from './styles'
+import {useState, KeyboardEvent} from 'react'
 
-export const AddArea = () => {
+type Props = {
+    onEnter : (taskName : string) => void
+}
+
+export const AddArea = ({onEnter} : Props) => {
+
+    const [inputText, setInputText] = useState ('');
+
+    const handleKeyUp = (e : KeyboardEvent) =>{
+
+        if (e.code === 'Enter' && inputText !== ''){
+            onEnter(inputText);
+            setInputText('')
+        }
+
+    }
+
     return(
         <C.container> 
             <div className="image"> ➕ </div>
             <input 
             type="text"
-            placeholder = "Adicionar tarefa" />
+            placeholder = "Adicionar tarefa" 
+            value = {inputText}
+            onChange = { e => setInputText(e.target.value)}
+            onKeyUp={handleKeyUp}
+            />
+           
 
         </C.container>
     );
