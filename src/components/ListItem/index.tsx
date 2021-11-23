06125c0bf0bc751fb2d3b1708f4d008050305ec5
Art <arthurluiz36@gmail.com>
2021-11-23 19:43:12 -0200

    funcao de rastreio de index e captura do estado do done

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,21 +1,33 @@
 import * as C from './styles'
 import {item} from '../../types/item'
-import { useState } from 'react'
+import { useState, KeyboardEvent } from 'react'
+
 
 type Props = {
     item: item;
+    taskId :(taskId : number, done: boolean) => void
 }
 
-export const ListItem = ({item} : Props) =>{
+export const ListItem = ({item , taskId} : Props) =>{
 
-    const [isChecked, setisChecked] = useState(item.done)
+    const [isChecked, setisChecked] = useState(item.done);
 
+    
+    const catchId = (checked : boolean) =>{
+
+        setisChecked (checked)
+        taskId (item.id - 1, checked)
+
+    }
+    
+    
     return(
          <C.container done = {isChecked}> 
             <input 
                 type ='checkbox'
                  checked={isChecked}
-                onChange={e => setisChecked(e.target.checked)}     
+                 onChange={e => catchId(e.target.checked)}   
+                 
             />
             <label> {item.name} </label> 
          </C.container>
